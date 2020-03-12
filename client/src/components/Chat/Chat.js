33c7544d6f8f5c1384df093e1a09382df6b0aa94
JustinKQuijanoo <quijanju@sheridancollege.ca>
2020-03-12 09:31:01 -0400

    Add error messages :exclamation:

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
-const Chat = () => {
+let socket; 
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect (() => {
+        const { name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, (error) => {
+            if(error) {
+                alert(error);
+            }
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    // function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
+
     return (
-        <h1>Chat</h1>
+        <div className="outer-container">
+            <div className="container"> 
+                <InfoBar room={room} />
+                <Messages 
+                    messages={messages}
+                    name={name}
+                />
+                <Input 
+                    message={message}
+                    setMessage={setMessage}
+                    sendMessage={sendMessage}
+                />
+                {/* <input 
+                value={message} 
+                onChange={(event) => setMessage(event.target.value)}
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} /> */}
+            </div>
+        </div>
+        
     )
 }
 
